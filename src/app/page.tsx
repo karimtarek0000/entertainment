4dce6c0fad76c8f3516858c51415d03784ed543a
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-06 14:30:55 +0300

    Refactor layout and globals styles to use Roboto font; update Home component for text size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-blue-500">
+    <div className="bg-blue-500 text-6xl">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, sed.
     </div>
   )
