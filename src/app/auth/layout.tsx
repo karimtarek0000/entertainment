aabb9fb3f0bd56823b504358271cf26ff06fed2c
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-07 15:11:07 +0300

    Remove unused SVG files and add Logo component for better structure in AuthLayout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,14 @@
+import Logo from '@/components/molecules/Logo'
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <main className="h-screen w-full flex-center">
-      <div className="bg-secondary w-full sm:w-[400px] rounded-[1.25rem] px-8">
+    <main className="h-screen w-full flex-col flex-center">
+      <Logo />
+      <div className="bg-secondary mt-20 w-full sm:w-[400px] rounded-[1.25rem] px-8">
         {children}
       </div>
     </main>
