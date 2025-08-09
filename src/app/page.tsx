d6b6d975a39195315a8cb9e8d0316ba0b438004a
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-09 16:56:45 +0300

    Make Home component asynchronous for improved data handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default async function Home() {
   return (
     <h1 className="bg-primary text-para-md text-white p-2">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, sed.
