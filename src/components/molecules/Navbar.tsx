0e3f6ac2f71702d83b657a5f28d01ce080cec0c9
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-10 16:09:17 +0300

    Add Navbar and RenderSVG components; integrate Navbar into Dashboard layout

---
+++ src/components/molecules/Navbar.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import RenderSVG from '@/components/molecules/RenderSVG'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const items = [
+  {
+    name: 'all',
+    href: '/dashboard',
+    icon: 'all',
+  },
+  {
+    name: 'movies',
+    href: '/dashboard/movies',
+    icon: 'movies',
+  },
+  {
+    name: 'series',
+    href: '/dashboard/series',
+    icon: 'series',
+  },
+  {
+    name: 'bookmarks',
+    href: '/dashboard/bookmarks',
+    icon: 'bookmarks',
+  },
+]
+
+export default function Navbar() {
+  const pathname = usePathname()
+
+  return (
+    <nav>
+      <ul className="flex-center flex-row lg:flex-col gap-6">
+        {items.map(item => {
+          const isActive = pathname === item.href
+          return (
+            <li key={item.name} role="button" aria-label={item.name}>
+              <Link href={item.href}>
+                <RenderSVG
+                  name={item.icon}
+                  className={`size-7 ${
+                    isActive ? 'fill-white' : 'fill-fourth'
+                  }`}
+                />
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
--- src/components/templates/Dashboard.tsx
+++ src/components/templates/Dashboard.tsx
@@ -1,7 +1,11 @@
+import Navbar from '@/components/molecules/Navbar'
+
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[96px_1fr]">
-      <header>navbar</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
     </div>
   )
