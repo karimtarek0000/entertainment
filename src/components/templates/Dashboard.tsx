ec73e612a42ec90421c42c2a0b8a3104fdea0157
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-10 17:13:52 +0300

    Update Dashboard layout: adjust grid column size for better responsiveness

--- src/components/templates/Dashboard.tsx
+++ src/components/templates/Dashboard.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/organisms/Header'
 
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[96px_1fr]">
+    <div className="grid grid-cols-1 lg:grid-cols-[6rem_1fr]">
       <Header />
       <main>{children}</main>
     </div>
