22b2628a3d57b5e9e5b7815ff91b71349b9ece27
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-11 14:40:13 +0300

    Refactor Card component: add type prop for better flexibility and improve structure

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -3,15 +3,36 @@
 import Button from '@/components/atoms/Button'
 import RenderSVG from '@/components/molecules/RenderSVG'
 
-export default function Card() {
+interface CardProps {
+  type: 'Movie' | 'TV Series'
+}
+
+const icons = {
+  Movie: 'movies',
+  'TV Series': 'series',
+}
+
+export default function Card({ type }: CardProps) {
   return (
-    <div className="bg-red-600 card">
-      <Button variant="secondary">
-        <RenderSVG
-          name="unactive-bookmark"
-          className="fill-transparent size-4"
-        />
-      </Button>
+    <div className="card-wrapper">
+      <div className="card">
+        <Button variant="secondary">
+          <RenderSVG
+            name="unactive-bookmark"
+            className="fill-transparent size-4"
+          />
+        </Button>
+      </div>
+      <div className="mt-2 capitalize">
+        <figure className="flex items-center gap-x-1 text-fifth text-[11px]">
+          2019 ·{' '}
+          <RenderSVG name={icons[type]} className="fill-fifth size-2.5" />{' '}
+          {type} · e
+        </figure>
+        <h2 className="mt-1 text-head-xs md:text-head-sm font-medium">
+          the great lands
+        </h2>
+      </div>
     </div>
   )
 }
