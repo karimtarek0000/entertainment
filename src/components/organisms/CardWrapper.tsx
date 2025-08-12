458a3cf38d8edb78973d6c57f98e92ae0e4d2296
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-12 15:01:34 +0300

    Refactor CardWrapper component: consolidate video control functions and update button implementation

--- src/components/organisms/CardWrapper.tsx
+++ src/components/organisms/CardWrapper.tsx
@@ -1,20 +1,22 @@
 'use client'
 
+import Button from '@/components/atoms/Button'
 import Card from '@/components/molecules/Card'
 import YouTubePlayer, {
   YouTubePlayerRef,
 } from '@/components/molecules/MediaPlayer'
+import RenderSVG from '@/components/molecules/RenderSVG'
 import { useRef } from 'react'
 
 export default function CardWrapper() {
   const playerRef = useRef<YouTubePlayerRef>(null)
 
-  const handlePlay = () => {
-    playerRef.current?.play()
-  }
-
-  const handlePause = () => {
-    playerRef.current?.pause()
+  const handleToggleVideo = () => {
+    if (playerRef.current?.isPlaying) {
+      playerRef.current?.pause()
+    } else {
+      playerRef.current?.play()
+    }
   }
 
   return (
@@ -25,27 +27,18 @@ export default function CardWrapper() {
             ref={playerRef}
             url="https://www.youtube.com/watch?v=Way9Dexny3w"
           />
+          <Button
+            onClick={handleToggleVideo}
+            variant="third"
+            className="group-hover/card:opacity-100"
+          >
+            <RenderSVG name="play" className="size-8" />
+            Play
+          </Button>
         </Card>
 
         {/* <SkeletonCard count={4} /> */}
       </div>
-
-      {/* <div className="flex flex-col items-center space-y-4">
-        <div className="flex space-x-4">
-          <button
-            onClick={handlePlay}
-            className="px-6 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-          >
-            Play
-          </button>
-          <button
-            onClick={handlePause}
-            className="px-6 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-          >
-            Pause
-          </button>
-        </div>
-      </div> */}
     </>
   )
 }
