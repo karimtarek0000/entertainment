e9d34a78682699c181e656bc23a720db560d9e3d
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-16 17:45:58 +0300

    Remove type prop from CardWrapper in Series component

--- src/app/dashboard/series/page.tsx
+++ src/app/dashboard/series/page.tsx
@@ -11,7 +11,7 @@ export default async function Series() {
       <h1 className="page-heading">TV Series</h1>
 
       <Suspense fallback={<SkeletonCard count={25} />}>
-        <CardWrapper type="TV Series" data={series} />
+        <CardWrapper data={series} />
       </Suspense>
     </section>
   )
