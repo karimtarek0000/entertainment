4314f176611830869730a68e9ddaf9fdf5f65438
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-16 16:27:19 +0300

    Refactor CardWrapper component to support dynamic video playback for multiple items

--- src/components/organisms/CardWrapper.tsx
+++ src/components/organisms/CardWrapper.tsx
@@ -8,37 +8,59 @@ import YouTubePlayer, {
 import RenderSVG from '@/components/molecules/RenderSVG'
 import { useRef, useState } from 'react'
 
-export default function CardWrapper() {
-  const playerRef = useRef<YouTubePlayerRef>(null)
-  const [isPlaying, setIsPlaying] = useState(false)
+interface CardWrapperProps {
+  data: {
+    id: string
+    title: string
+    trailer: string
+    thumbnail: string
+  }[]
+}
 
-  const handleToggleVideo = () => {
-    if (playerRef.current?.isPlaying) {
-      setIsPlaying(false)
-      playerRef.current?.pause()
-    } else {
-      setIsPlaying(true)
-      playerRef.current?.play()
+export default function CardWrapper({ data }: CardWrapperProps) {
+  const playerRefs = useRef<{ [key: string]: YouTubePlayerRef | null }>({})
+  const [playingStates, setPlayingStates] = useState<{
+    [key: string]: boolean
+  }>({})
+
+  const handleToggleVideo = (id: string) => {
+    const player = playerRefs.current[id]
+
+    if (player) {
+      const isCurrentlyPlaying = playingStates[id] || false
+
+      if (isCurrentlyPlaying) {
+        player.pause()
+        setPlayingStates(prev => ({ ...prev, [id]: false }))
+      } else {
+        player.play()
+        setPlayingStates(prev => ({ ...prev, [id]: true }))
+      }
     }
   }
 
   return (
     <div className="card-wrapper">
-      <Card type="Movie">
-        <YouTubePlayer
-          ref={playerRef}
-          url="https://www.youtube.com/watch?v=Way9Dexny3w"
-          imageURL="/test.jpg"
-        />
-        <div className="card-wrapper__actions">
-          <Button onClick={handleToggleVideo} variant="third" className="">
-            <RenderSVG name={isPlaying ? 'pause' : 'play'} className="size-8" />
-            {isPlaying ? 'Pause' : 'Play'}
-          </Button>
-        </div>
-      </Card>
-
-      {/* <SkeletonCard count={4} /> */}
+      {data?.map(item => (
+        <Card key={item.id} type="Movie" title={item.title}>
+          <YouTubePlayer
+            ref={ref => {
+              playerRefs.current[item.id] = ref
+            }}
+            url={item.trailer}
+            imageURL={item.thumbnail}
+          />
+          <div className="card-wrapper__actions">
+            <Button onClick={() => handleToggleVideo(item.id)} variant="third">
+              <RenderSVG
+                name={playingStates[item.id] ? 'pause' : 'play'}
+                className="size-8"
+              />
+              {playingStates[item.id] ? 'Pause' : 'Play'}
+            </Button>
+          </div>
+        </Card>
+      ))}
     </div>
   )
 }
