5cfc6a9cc203e0efcb6da11db07ba22d30e18fa2
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-16 17:46:06 +0300

    Add getCategories action to fetch and categorize trending and recommended movies and TV series

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -9,3 +9,34 @@ export const getSeries = async () => {
   const data = await fetch(`${process.env.API_URL}/tvSeries`)
   return await data.json()
 }
+
+export const getCategories = async () => {
+  const [
+    trendingTvSeries,
+    recommendedTvSeries,
+    trendingMovies,
+    recommendedMovies,
+  ] = await Promise.all([
+    fetch(`${process.env.API_URL}/tvSeries?category=trending`),
+    fetch(`${process.env.API_URL}/tvSeries?category=recommended`),
+    fetch(`${process.env.API_URL}/movies?category=trending`),
+    fetch(`${process.env.API_URL}/movies?category=recommended`),
+  ])
+
+  const [
+    trendingTvData,
+    recommendedTvData,
+    trendingMovieData,
+    recommendedMovieData,
+  ] = await Promise.all([
+    trendingTvSeries.json(),
+    recommendedTvSeries.json(),
+    trendingMovies.json(),
+    recommendedMovies.json(),
+  ])
+
+  return {
+    trending: [...trendingTvData, ...trendingMovieData],
+    recommended: [...recommendedTvData, ...recommendedMovieData],
+  }
+}
