05072ef756b3bbd06af237902547a9329ddf9d8e
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-16 17:10:41 +0300

    Implement Series component to fetch and display TV series with loading state

--- src/app/dashboard/series/page.tsx
+++ src/app/dashboard/series/page.tsx
@@ -1,3 +1,18 @@
-export default function Series() {
-  return <div>Series</div>
+import { getSeries } from '@/actions'
+import SkeletonCard from '@/components/molecules/skeleton/SkeletonCard'
+import CardWrapper from '@/components/organisms/CardWrapper'
+import { Suspense } from 'react'
+
+export default async function Series() {
+  const series = await getSeries()
+
+  return (
+    <section className="container">
+      <h1 className="page-heading">TV Series</h1>
+
+      <Suspense fallback={<SkeletonCard count={25} />}>
+        <CardWrapper type="TV Series" data={series} />
+      </Suspense>
+    </section>
+  )
 }
