4a458ef8943f1fc4df08fd94d34a87ae5a1a41fd
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-16 17:10:52 +0300

    Add getSeries action to fetch TV series data from API

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,3 +4,8 @@ export const getMovies = async () => {
   const data = await fetch(`${process.env.API_URL}/movies`)
   return await data.json()
 }
+
+export const getSeries = async () => {
+  const data = await fetch(`${process.env.API_URL}/tvSeries`)
+  return await data.json()
+}
