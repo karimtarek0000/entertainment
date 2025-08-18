09aa2e0963d2ab2ff2d332d8b2f915c92dbff5cc
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-18 15:43:55 +0300

    Fix import path for getSeries in Series component and update data type in CardWrapper

--- src/app/dashboard/series/page.tsx
+++ src/app/dashboard/series/page.tsx
@@ -1,4 +1,4 @@
-import { getSeries } from '@/actions'
+import { getSeries } from '@/actions/resourcess'
 import SkeletonCard from '@/components/molecules/skeleton/SkeletonCard'
 import CardWrapper from '@/components/organisms/CardWrapper'
 import { Suspense } from 'react'
@@ -11,7 +11,7 @@ export default async function Series() {
       <h1 className="page-heading">TV Series</h1>
 
       <Suspense fallback={<SkeletonCard count={25} />}>
-        <CardWrapper data={series} />
+        <CardWrapper data={series as []} />
       </Suspense>
     </section>
   )
