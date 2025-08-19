91c0856120ce41a58dc5232e6b15a3d1c6e63699
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-19 17:02:19 +0300

    Refactor Bookmarks component to accept searchParams and query for fetching bookmarks

--- src/app/dashboard/bookmarks/page.tsx
+++ src/app/dashboard/bookmarks/page.tsx
@@ -4,16 +4,18 @@ import SkeletonCard from '@/components/molecules/skeleton/SkeletonCard'
 import CardWrapper from '@/components/organisms/CardWrapper'
 import { Suspense } from 'react'
 
-export default async function Bookmarks() {
-  const bookmarks = await getBookmarks()
+export default async function Bookmarks({ searchParams }: PageProps) {
+  const sp = await searchParams
+  const query = sp.search || ''
+  const bookmarks = await getBookmarks(query)
 
   return (
     <section className="container">
       <h1 className="page-heading">Bookmarks</h1>
 
       <Suspense fallback={<SkeletonCard count={10} />}>
-        <NotFoundContent data={bookmarks} title="No bookmarks available">
-          <CardWrapper data={bookmarks} />
+        <NotFoundContent data={bookmarks as []} title="No bookmarks available">
+          <CardWrapper data={bookmarks as []} />
         </NotFoundContent>
       </Suspense>
     </section>
