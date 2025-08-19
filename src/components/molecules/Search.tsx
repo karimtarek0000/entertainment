2db7bfa09246fbffe18e04a7751285c05820e8ab
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-08-19 15:01:17 +0300

    Refactor Search component to implement debounced search functionality

--- src/components/molecules/Search.tsx
+++ src/components/molecules/Search.tsx
@@ -2,7 +2,9 @@
 
 import Input from '@/components/atoms/Input'
 import RenderSVG from '@/components/molecules/RenderSVG'
-import { usePathname } from 'next/navigation'
+import { useDebounce } from '@/hooks/Debounce'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { ChangeEvent } from 'react'
 
 const placeHolders: Record<string, string> = {
   '/dashboard': 'movies or TV series',
@@ -13,14 +15,22 @@ const placeHolders: Record<string, string> = {
 
 export default function Search() {
   const pathname = usePathname()
+  const { replace } = useRouter()
+  const searchParams = useSearchParams()
+
+  const searchHandler = useDebounce((e: ChangeEvent<HTMLInputElement>) => {
+    replace(`${pathname}?search=${e.target.value}`)
+  }, 1000)
 
   return (
     <form className="mt-6 flex items-end space-x-1 ps-4 max-h-8 mb-6">
       <RenderSVG name="search" className="mb-3" />
       <Input
+        onChange={e => searchHandler(e)}
         className="border-transparent focus:border-transparent"
         aria-label={`Search for ${placeHolders[pathname]}`}
         placeholder={`Search for ${placeHolders[pathname]}`}
+        defaultValue={searchParams.get('search')?.toString()}
       />
     </form>
   )
